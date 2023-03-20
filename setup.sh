dbedeb384136272d1ea6a6cfda490d218dd2b332
tha dddd <99595273+thadddd@users.noreply.github.com>
2023-03-20 14:12:59 +0000

    032023 - update

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@
 ###                                                               made by: thadddd ###
 ###                                                 http://www.github.com/thadddd/ ###
 ###                                                        Started: March 10, 2023 ###
-###                                  Current v: 1.00   Upload date: March 16, 2023 ### ######################################################################################
+###                                  Current v: 1.00   Upload date: March 16, 2023 ### 
+######################################################################################
 
 ## Nvidia GeForce 560 Ti installer with Cuda 8.0 on Ubuntu Server 20.04.3
 
@@ -19,7 +20,7 @@ suap='sudo apt'
 suag='sudo apt-get'
 su='sudo'
 upda='sudo apt update'
-line='printf'
+line='echo -ne'
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -84,25 +85,22 @@ step_1(){
         ";
         $line;
     header2;
-    $line;
-    sleep 3;
-    pause;
-    $line;
+                pause;
         $suap update;
-    header2;
+                spacer;
+                spacer;
         $suap full-upgrade -y;
-    header2;
+                spacer;
+                spacer;
         $suap autoremove -y;
-    header2;
-    $line;
-        $inst p7zip-full p7zip-rar aptitude screen ubuntu-drivers-common timeshift net-tools;
-        $line;
-            $inst build-essential;
-    header2;
-    $line;
+                spacer;
+                spacer;
+        $inst p7zip-full p7zip-rar aptitude screen ubuntu-drivers-common timeshift net-tools dkms build-essential;
+                spacer;
+                spacer;
     $su timeshift --create --comment "Step1 Finish";
     footer1;
-    pause;
+                pause;
 }
 
 step_2(){
@@ -111,44 +109,44 @@ step_2(){
         $red==$noc$grn STEP 2 -$noc$ylw Install Some Crap        $noc$red   ==$noc
         ";
     header2;
-    $line;
+                spacer;
+                spacer;
     sleep 3;
         $su chmod ugo+rwx /etc/apt -R;
         $su mv -f sources.list.bk /etc/apt/sources.list.bk;
-        $su cp -f /etc/apt/trusted.gpg /etc/apt/trusted.gpg.bk;
-    header2;
-    $line;
+                spacer;
+                spacer;
         echo deb http://us.archive.ubuntu.com/ubuntu/ xenial main | $su tee -a /etc/apt/sources.list;
         echo deb http://us.archive.ubuntu.com/ubuntu/ xenial universe | $su tee -a /etc/apt/sources.list;
-        $su apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 40976EAF437D05B5 3B4FE6ACC0B21F32;
+            $su apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 40976EAF437D05B5 3B4FE6ACC0B21F32;
         $suag update && $suap update; 
-    header2;
-    $line;
+                spacer;
+                spacer;
         $inst gcc-5 g++-5;
-    header2;
-    $line;
-        $su cp -f /etc/apt/sources.list.bk /etc/apt/sources.list;
-        $su cp -f /etc/apt/trusted.gpg.bk /etc/apt/trusted.gpg;
-        $su apt-key update;
-        $suag update && $suap update;
-    header2;
-    $line;
+                spacer;
+                spacer;
+            $su cp -f /etc/apt/sources.list.bk /etc/apt/sources.list;
+            $su cp -f /etc/apt/trusted.gpg.bk /etc/apt/trusted.gpg;
+            $su apt-key update;
+                    $suag update && $suap update;
+                spacer;
+                spacer;
         cd /opt/ || return;
-        $su mkdir gcc5;
+            $su mkdir gcc5;
         cd gcc5 || return;
-        $su ln -s /usr/bin/gcc-5 gcc;
-        $su ln -s /usr/bin/g++-5 g++;
-        export PATH=/opt/gcc5:$PATH;
-        $su chmod ugo+rwx /etc/modprobe.d/ -R;
-        touch /etc/modprobe.d/blacklist-nouveau.conf;
+            $su ln -s /usr/bin/gcc-5 gcc;
+            $su ln -s /usr/bin/g++-5 g++;
+                    export PATH=/opt/gcc5:$PATH;
+            $su chmod ugo+rwx /etc/modprobe.d/ -R;
+                    touch /etc/modprobe.d/blacklist-nouveau.conf;
         echo blacklist nouveau | $su tee -a /etc/modprobe.d/blacklist-nouveau.conf;
         echo options nouveau modeset=0 | $su tee -a /etc/modprobe.d/blacklist-nouveau.conf;
-        $su update-initramfs -u;
-    header2;
-    $line;
-        $su timeshift --create --comments "Step2 Finsish";
-        footer1;
-    pause;
+            $su update-initramfs -u;
+                spacer;
+                spacer;
+            $su timeshift --create --comments "Step2 Finsish";
+    footer1;
+                pause;
 }
 
 step_3(){
@@ -158,49 +156,56 @@ step_3(){
         $red==$noc$grn STEP 3 -$noc$ylw Purge Nvida drivers   4noc$red      ==$noc
     ";
     header2;
-    $su nvidia-uninstall;
-    header2;
-    $su nvidia-installer --uninstall;
-    header2;
-    $suap remove --purge '^nvidia-.*';
-    header2;
-    $su timeshift --create --comments "Step3 Finish";
+        $su nvidia-uninstall;
+                spacer;
+                spacer;
+        $su nvidia-installer --uninstall;
+                spacer;
+                spacer;
+        $suap remove --purge '^nvidia-.*';
+                spacer;
+                spacer;
+        $su timeshift --create --comments "Step3 Finish";
     footer1;
-    pause;
+                pause;
 }
 
 step_4(){
     header1;
     echo -e "$red==$noc$grn STEP 4 -$noc$ylw Install Nvidia and Cuda $noc$red    ==$noc";
     header2;
-    $su service lightdm stop;
-    $su service gdm3 stop;
-    $su killall Xorg;
-    header2;
-    $su ln -s /usr/bin/gcc-5 gcc;
-    $su ln -s /usr/bin/g++-5 g++;
-    export PATH=/opt/gcc5:$PATH;
-    $su mkdir /home/cuda-8.0;
-    $su mkdir /home/cuda-8.0/dl;
-    $su chmod ugo+rwx /home -R;
-    cd /home/cuda-8.0/dl || return;
+        $su service lightdm stop;
+        $su service gdm3 stop;
+        $su killall Xorg;
+                spacer;
+                spacer;
+        $su ln -s /usr/bin/gcc-5 gcc;
+        $su ln -s /usr/bin/g++-5 g++;
+            export PATH=/opt/gcc5:$PATH;
+        $su mkdir /home/cuda-8.0;
+        $su mkdir /home/cuda-8.0/dl;
+            $su chmod ugo+rwx /home -R;
+            cd /home/cuda-8.0/dl || return;
     wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run;
     wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/patches/2/cuda_8.0.61.2_linux-run;
-    $su sh cuda_8.0.61_375.26_linux-run --tar mxvf;
-    $su cp InstallUtils.pm /usr/lib/x86_64-linux-gnu/perl-base/;
-    header2;
-    $su modprobe -r nouveau;
+        $su sh cuda_8.0.61_375.26_linux-run --tar mxvf;
+        $su cp InstallUtils.pm /usr/lib/x86_64-linux-gnu/perl-base/;
+                spacer;
+                spacer;
+        $su modprobe -r nouveau;
     #$inst nvidia-driver-390 nvidia-headless-390 nvidia-utils-390;
     #$su modprobe -i nvidia;
-    sh cuda_8.0.61_375.26_linux-run;
-    header2; sleep 2;
-    $su sh cuda_8.0.61.2_linux-run;
-    $su modprobe -i nvidia
-    #$su aptitude build-dep nvidia-smi;
-    $inst nvidia-smi;
-    $su timeshift --create --comments "Step4 Finish";
+        $su sh cuda_8.0.61_375.26_linux-run --override;
+                spacer;
+                spacer;
+                    sleep 2;
+        $su sh cuda_8.0.61.2_linux-run;
+        $su modprobe -i nvidia
+                #$su aptitude build-dep nvidia-smi;
+        $inst nvidia-smi;
+        $su timeshift --create --comments "Step4 Finish";
     footer1;
-    pause;
+                pause;
 }
 
 step_5(){
@@ -209,37 +214,50 @@ step_5(){
         $red==$noc$grn STEP 5 -$noc$ylw Install Support Programs $noc$red   ==$noc 
         ";
     header2;
-    $upda;
-    $inst hashcat hashcat-nvidia;
-    $inst mokutil;
-    $inst libelf-dev;
-    $inst wireshark;
-    $inst hcxtools;
+        $su chmod ug+rwx /home -R;
+        cd /home || return;
+            $upda;
+        $inst hashcat hashcat-nvidia;
+                spacer;
+                spacer;
+        $inst mokutil;
+                spacer;
+                spacer;
+        $inst libelf-dev;
+                spacer;
+                spacer;
+        $inst wireshark;
+                spacer;
+                spacer;
+        $su git clone https://github.com/ZerBea/hcxtools.git
+            cd hcxtools || return;
+            make; make install;
+                spacer;
+                spacer;
     hashcat -I;
-    pause;
+                pause;
     hashcat -b;
-    pause;
-    $su timeshift --create --comments "Step5 Finish" ;
+                pause;
     footer1;
-    pause;
+                pause;
 }
 
 step_6(){
-    clear;
+        clear;
     echo -ne "
         $red XXXXXXXXXXX NOT SETUP YET XXXXXXXXXXXXXX" ;
-    pause;
+                pause;
 }
 
 header1(){
-    clear;
+        clear;
     echo -ne "$red==========================================$noc";
 }
 
 header2(){
     echo -ne "$red==========================================$noc";
     echo " ";
-    pause;
+                pause;
 }
 
 footer1(){
@@ -247,14 +265,17 @@ footer1(){
     $red=====$noc$ylw DONE$noc$red =====$noc"
 }
 
-pause(){
-    $line;
-    while read -r -t 0.001; do :; done # dump the buffer
-        echo -e "
-        $red" Press "$grn"any "$ylw"key "$mag"to "$blu"continue "$noc"
-            read -n1 -rsp ' ';
-    $line;
+spacer(){
+    echo -ne "$red==========================================$noc";
 }
+pause(){
+                spacer;
+    while read -r -t 0.001; do :; done # dump the buffer
+        echo -e "$red" Press "$grn"any "$ylw"key "$mag"to "$blu"continue "$noc"
+            read -n1 -rsp ' ';
+                spacer;
+}
+
 
 # # # # # # # # # # # # # # # # # # # # # # # # # #
 #   PROGRAM
